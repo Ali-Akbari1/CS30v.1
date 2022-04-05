7ad8c1a1277ee2805c5170a1594848869c772c4f
904086002 <904086002@L815WD220002.edu.cbe.ab.ca>
2022-04-05 10:26:02 -0600

    april 5

--- Chapter11/src/StatsPackage/Stats.java
+++ Chapter11/src/StatsPackage/Stats.java
@@ -9,30 +9,40 @@ public class Stats
 	public static void main (String[] args) 
 	{
 		
-		File dataFile = new File("test1.dat");
+		File dataFile = new File("test1.txt");
 		FileReader in;
 		BufferedReader readFile;
-		String score;
+		String name;
 		double avgScore;
 		double totalScores = 0;
+		double score = 0;
 		int numScores = 0;
+		double g1 = 0;
+		
+		
 		
 		try 
 		{
 			in = new FileReader(dataFile);
 			readFile = new BufferedReader(in);
-			while((score = readFile.readLine()) != null) 
+			
+			
+			while((name = readFile.readLine()) != null) 
 			{
 				numScores +=1;
-				System.out.println(score);
-				totalScores += Double.parseDouble(score);
+				System.out.println(name);
+				//totalScores += Double.parseDouble(name);
 				
 			}
+			//g1 = Double.parseDouble(info[1].toString());
 			avgScore = totalScores / numScores; 
 			System.out.println("Average Score =   "+ avgScore);
 			readFile.close();
 			in.close();
 		} 
+		
+		
+		
 		catch(FileNotFoundException e) 
 		{
 			System.out.println("File does not exist or could not be found");
